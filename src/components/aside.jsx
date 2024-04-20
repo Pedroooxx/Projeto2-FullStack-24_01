deb0b84046d4d803e75ffe47bbc4ab4f1136b2b6
Pedro de Oliveira Machado <pedromachado.2003@alunos.utfpr.edu.br>
2024-04-20 17:28:35 -0300

    testing

--- src/components/aside.jsx
+++ src/components/aside.jsx
@@ -1,5 +1,6 @@
 import aside from "./stylesheets/aside.css"
 import characterImg from "../images/character.png";
+import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 
@@ -9,8 +10,8 @@ const Aside = () => {
     <div className="aside">
       <h1 className="searchTitle">Seletor de Personagem</h1>
       <p class="searchSubTitle">Para buscar um personagem basta digitar seu <span className="importantInfo">nome</span> ou sua <span className="importantInfo">ID</span>. </p>
-      <Button variant="outlined">Buscar</Button>;
-
+      <TextField sx={{ color: "green"}} variant="standard" />
+      <Button sx={{ color: "black", bgcolor: "rgb(193, 237, 48)"}} variant="contained">Contained</Button>
 
     </div>
   );
